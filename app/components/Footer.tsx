818f62e5c5f2a1f3c110dd8c50443829b1cf0a48
Meyangz <Kayfolayan614@gmail.com>
2024-04-01 21:01:06 +0100

    done with the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import {
+  FaInstagram,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaMailBulk,
+  FaMap,
+  FaMapMarker,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaPhoneAlt,
+} from "react-icons/fa";
+import {
+  FaLinkedinIn,
+  FaTwitter,
+  FaFacebook,
+  FaFacebookF,
+} from "react-icons/fa6";
 
 const Footer = () => {
   const [date, setDate] = useState(0);
@@ -13,7 +30,7 @@ const Footer = () => {
   return (
     <div className="w-full sm:w-full place-self-end bg-slate-300 ">
       <div className="p-10  flex justify-center gap-5 md:gap-28">
-        <div className="">
+        <div className="hidden lg:block">
           <Link href="/">
             <Image
               className="w-28"
@@ -24,83 +41,89 @@ const Footer = () => {
             />
           </Link>
         </div>
-        <div className="grid md:flex gap-[200px] ">
-          <div className="grid gap-7">
+        <div className="grid gap-10 md:flex md:gap-[100px]  lg:gap-[150px] ">
+          <div className="flex flex-col  gap-5 md:gap-7">
             <p className="w-fit border-b-4 border-slate-900 text-lg">Company</p>
-            <div className="grid gap-5">
+            <div className="grid gap-2 md:gap-5">
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-500 whitespace-nowrap transition duration-200"
                 href="#"
               >
                 How we operate
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-500 whitespace-nowrap transition duration-200"
                 href="#"
               >
                 Our courses
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-500 whitespace-nowrap transition duration-200"
                 href="#"
               >
                 Upcoming programs
               </Link>
             </div>
           </div>
-          <div className="grid gap-7">
+          <div className="flex flex-col gap-5 md:gap-7">
             <p className="w-fit border-b-4 border-slate-900 text-lg">
               Contact Us
             </p>
-            <div className="grid gap-5">
+            <div className="grid md:gap-2 gap-5">
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="flex items-center gap-2 hover:text-slate-500 transition duration-200"
                 href="#"
               >
-                How we operate
+                <FaPhoneAlt /> 09137437424
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="flex items-center gap-2 hover:text-slate-500 transition duration-200"
                 href="#"
               >
-                Our courses
+                <FaMapMarkerAlt /> lugbe,Abuja,Nigeria
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
-                href="#"
+                className="flex items-center gap-2 hover:text-slate-500 transition duration-200"
+                href="mailto:israelvictor126@gmail.com"
               >
-                Upcoming programs
+                <FaMailBulk /> israelvictor126@gmail.com
               </Link>
             </div>
           </div>
-          <div className="grid gap-7">
+          <div className="flex flex-col gap-7">
             <p className="w-fit border-b-4 border-slate-900 text-lg">
               Follow Up
             </p>
-            <div className="grid gap-5">
+            <div className="flex text-2xl gap-5 text-slate-800">
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-600 transition duration-150 ease-in-out"
                 href="#"
               >
-                How we operate
+                <FaFacebook />
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-600 transition duration-150 ease-in-out"
                 href="#"
               >
-                Our courses
+                <FaLinkedin />
               </Link>
               <Link
-                className="hover:text-slate-500 transition duration-200"
+                className="hover:text-slate-600 transition duration-150 ease-in-out"
                 href="#"
               >
-                Upcoming programs
+                <FaInstagramSquare />
+              </Link>
+              <Link
+                className="hover:text-slate-600 transition duration-150 ease-in-out"
+                href="#"
+              >
+                <FaTwitter />
               </Link>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full h-[40px] flex justify-center items-center text-white bg-gray-500  ">
+      <div className="w-full h-[40px] flex justify-center items-center text-white bg-gray-700  ">
         <p>© {date ? date : ""} MI-TECH. All rights reserved</p>
       </div>
     </div>
